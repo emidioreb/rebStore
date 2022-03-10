24f04086fa200c241a0b7311e10b91221f63b8ee
emidioreb <emidioreb@gmail.com>
2022-03-09 22:00:15 -0300

    alterando css do login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import '../css/style.css';
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-all">
+      <div className="image-login">
+        <h1>imagem</h1>
+      </div>
+      <div className="login-screen">
+        <div className="login">
+          <h5>Seja Bem vindo</h5>
+          <label htmlFor="email" className="email-login">
+            E-mail
+            <br />
+            <input
+              type="email"
+              name="email"
+              // value={inicialContribution}
+              // onChange={({ target }) => setInicialContribution(target.value)}
+            />
+          </label>
+          <br />
+          <label htmlFor="password" className="password-login">
+            Senha
+            <br />
+            <input
+              type="password"
+              name="password"
+              // value={inicialContribution}
+              // onChange={({ target }) => setInicialContribution(target.value)}
+            />
+          </label>
+          <br />
+          <button
+            type="button"
+            className="enter"
+            // onClick={clearInput}
+          >
+            Entrar
+          </button>
+          <p>Ainda não possui cadastro ? Cadastra-se</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,61 @@ import React from 'react';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <div>
+        <div>
+          <h1>imagem</h1>
+        </div>
+        <div>
+          <div className="register-screen">
+            <h5>Cadastra-se</h5>
+            <label htmlFor="fullName" className="full-name">
+              Nome completo
+              <br />
+              <input
+                type="text"
+                name="fullName"
+              />
+            </label>
+            <br />
+            <label htmlFor="email" className="email-register">
+              E-mail
+              <br />
+              <input
+                type="email"
+                name="email"
+              />
+            </label>
+            <br />
+            <label htmlFor="password" className="password-register">
+              Senha
+              <br />
+              <input
+                type="password"
+                name="password"
+              />
+            </label>
+            <br />
+            <label htmlFor="confirmPassword" className="confirm-Password-register">
+              Confirmar senha
+              <br />
+              <input
+                type="password"
+                name="confirmPassword"
+              />
+            </label>
+            <br />
+            <button
+              type="button"
+              className="enter-register"
+            >
+              Entrar
+            </button>
+            <p>Já possui cadastro ? Login</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
